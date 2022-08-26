73a6dabd49034affe8a06e1452a68b851aa2b01c
Jamalam - James <james@jamalam.tech>
2022-08-26 19:45:54 +0100

    feat: it's a suprise tool that will help us later

---
+++ src/main/java/io/github/jamalam360/utility/belt/mixin/Plugin.java
@@ -0,0 +1,50 @@
+package io.github.jamalam360.utility.belt.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Jamalam
+ */
+public class Plugin implements IMixinConfigPlugin {
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.contains(".compatibility.")) {
+            String modId = mixinClassName.split(".compatibility.")[1].split("\\.")[0];
+            return FabricLoader.getInstance().isModLoaded(modId);
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
