7b5d62d0f34403159b29e3d244a59c3fbe73e292
James (Jamalam) <james@jamalam.tech>
2022-10-28 10:42:39 +0100

    fix: ensure we are dealing with mainhand when tools break, closes #15

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,13 +2,8 @@
 
 ### Features
 
-- Add a new config option to allow you to change the behaviour when using hotbar keys in the belt.
-- Use JamLib mouse callbacks.
-
 ### Fixes
 
-- Attempt to fix a mysterious `ArrayIndexOutOfBoundsException`. (#13, #14)
+Closed issues: None.
 
-Closed issues: #12, #13, #14.
-
-[Full Changelog](https://github.com/JamCoreModding/UtilityBelt/compare/1.1.1...1.1.2)
+[Full Changelog](https://github.com/JamCoreModding/UtilityBelt/compare/...)
--- src/main/java/io/github/jamalam360/utility/belt/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/jamalam360/utility/belt/mixin/LivingEntityMixin.java
@@ -36,6 +36,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -55,6 +56,8 @@ public abstract class LivingEntityMixin implements Ducks.LivingEntity {
             at = @At("HEAD")
     )
     private void utilitybelt$updateUtilityBeltNbtOnToolBreak(EquipmentSlot slot, CallbackInfo ci) {
+        if (slot != EquipmentSlot.MAINHAND) return;
+
         if (((LivingEntity) (Object) this) instanceof PlayerEntity player) {
             if (TrinketsUtil.hasUtilityBelt(player)) {
                 ItemStack utilityBelt = TrinketsUtil.getUtilityBelt(player);
