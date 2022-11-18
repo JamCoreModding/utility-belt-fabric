a425514388e35dd6b06d0df33315545747c7dd20
James (Jamalam) <james@jamalam.tech>
2022-11-18 08:43:52 +0000

    fix: move belt up

--- src/main/java/io/github/jamalam360/utility/belt/client/BeltModel.java
+++ src/main/java/io/github/jamalam360/utility/belt/client/BeltModel.java
@@ -41,13 +41,12 @@ public class BeltModel extends BipedEntityModel<LivingEntity> {
     public static TexturedModelData createTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData root = modelData.getRoot();
-
         ModelPartData body = root.addChild(
                 EntityModelPartNames.BODY,
-                ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, 11.0F, -3.0F, 10.0F, 2.0F, 6.0F, new Dilation(0.0F))
-                        .uv(0, 8).cuboid(-4.0F, 12.0F, -4.0F, 4.0F, 4.0F, 1.0F, new Dilation(0.0F))
-                        .uv(10, 8).cuboid(1.0F, 12.0F, -4.0F, 3.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.rotation(0.0F, 0.0F, 0.0F));
-
+                ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, 10.0F, -3.0F, 10.0F, 2.0F, 6.0F, new Dilation(0.0F))
+                        .uv(0, 8).cuboid(-4.0F, 11.0F, -4.0F, 4.0F, 4.0F, 1.0F, new Dilation(0.0F))
+                        .uv(10, 8).cuboid(1.0F, 11.0F, -4.0F, 3.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.rotation(0.0F, 0.0F, 0.0F));
+        body.addChild("pouch", ModelPartBuilder.create().uv(18, 8).cuboid(0.0F, -13.0F, -6.0F, 2.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 24.0F, 0.0F, 0.0F, 1.5708F, 0.0F));
 
         root.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create(), ModelTransform.NONE);
         root.addChild(EntityModelPartNames.HAT, ModelPartBuilder.create(), ModelTransform.NONE);
@@ -55,12 +54,6 @@ public class BeltModel extends BipedEntityModel<LivingEntity> {
         root.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create(), ModelTransform.NONE);
         root.addChild(EntityModelPartNames.RIGHT_LEG, ModelPartBuilder.create(), ModelTransform.NONE);
         root.addChild(EntityModelPartNames.LEFT_LEG, ModelPartBuilder.create(), ModelTransform.NONE);
-
-//        ModelPartData belt = body.addChild("belt", ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, 11.0F, -3.0F, 10.0F, 2.0F, 6.0F, new Dilation(0.0F))
-//                .uv(0, 8).cuboid(-4.0F, 12.0F, -4.0F, 4.0F, 4.0F, 1.0F, new Dilation(0.0F))
-//                .uv(10, 8).cuboid(1.0F, 12.0F, -4.0F, 3.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.rotation(0.0F, 0.0F, 0.0F));
-
-        body.addChild("pouch", ModelPartBuilder.create().uv(18, 8).cuboid(0.0F, -12.0F, -6.0F, 2.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 24.0F, 0.0F, 0.0F, 1.5708F, 0.0F));
 
         return TexturedModelData.of(modelData, 64, 64);
     }
